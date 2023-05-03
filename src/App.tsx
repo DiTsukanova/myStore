d7030b6e76ee0f192e4b23d88fcf9d7c9d922cfd
DiTsukanova <78157935+DiTsukanova@users.noreply.github.com>
2023-05-03 22:13:19 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <div className="content">
           <div className="container">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/myStore" element={<Home />} />
               <Route path="*" element={<NotFound />} />
               <Route path="/pizza/:id" element={<FullPizza/>} />
               <Route path="/cart.html" element={<Cart />} />
